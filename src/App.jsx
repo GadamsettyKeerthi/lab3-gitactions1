1ded07dbdd46798251dfaeb6c01f1a596a067faa
Keerthi <2300030185@kluniversity.in>
2025-10-16 23:38:18 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+
+function App() {
+
+
+  return (
+      <div>
+        <h1>Welcome to DEVOPS</h1>
+        <p>practice lab 3 - git actions</p>
+      </div>
+  )
+}
+
+export default App
