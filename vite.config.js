0238552dbf2f82341db1419df9fa2e7cfaac6358
Keerthi <2300030185@kluniversity.in>
2025-10-16 23:57:47 +0530

    added files

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Lab3-gitactions1/',
+  base:'/lab3-gitactions1/',
 
 })
